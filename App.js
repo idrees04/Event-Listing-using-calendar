05584cd12d732b34ca12c9b0272b73fae934e29a
idrees04 <idreesar@yahoo.com>
2021-09-14 23:41:56 +0500

    layout ready for two pages

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button, View, Text } from "react-native";
+import eventListing from "./screen/EventListingScreen";
+import CreateEvent from "./screen/CreateEventScreen";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="EventScreen">
+        <Stack.Screen name="EventScreen" component={eventListing} />
+        <Stack.Screen name="Create" component={CreateEvent} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
